47c8fc5b8771356ff688ae65c5025c08a316c44c
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-06-25 16:45:18 +0900

    Feat: 회원가입 아이디, 이메일 검증

--- client/src/pages/auth/Signup.jsx
+++ client/src/pages/auth/Signup.jsx
@@ -1,10 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Container } from '../../style/Container';
 
 export default function Signup() {
+  const [values, setValues] = useState({
+    userId: '',
+    userPw: '',
+    confirmPw: '',
+    email: '',
+    code: '',
+  });
+
+  const hadleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+    console.log(values);
+  };
+
+  const validId = async () => {
+    console.log('중복확인 클릭');
+    const { userId } = values;
+    try {
+      const { data } = await axios.post('http://localhost:5000/api/users/userIdvaild', {
+        userId,
+      });
+      console.log(data);
+    } catch (error) {
+      console.error('Error validating user ID:', error);
+      console.log('서버 error');
+    }
+  };
+
+  const validEmail = async () => {
+    console.log('중복확인 클릭');
+    const { email } = values;
+    try {
+      const { data } = await axios.post('http://localhost:5000/api/users/emailvalid', {
+        email,
+      });
+      console.log(data);
+    } catch (error) {
+      console.error('Error validating user ID:', error);
+      console.log('서버 error');
+    }
+  };
+
   return (
     <>
-      <Container>회원가입 페이지 입니다.</Container>
+      <Container>
+        <form>
+          <label>
+            아이디
+            <input
+              type='text'
+              name='userId'
+              onChange={(e) => hadleChange(e)}
+              autoComplete='username'
+            />
+          </label>
+          <button type='button' onClick={validId}>
+            중복확인
+          </button>
+          <label>
+            비밀번호
+            <input
+              type='password'
+              name='userPw'
+              onChange={(e) => hadleChange(e)}
+              autoComplete='new-password'
+            />
+          </label>
+          <label>
+            비밀번호 확인
+            <input
+              type='password'
+              name='confirmPw'
+              onChange={(e) => hadleChange(e)}
+              autoComplete='new-password'
+            />
+          </label>
+          <label>
+            이메일
+            <input type='email' name='email' onChange={(e) => hadleChange(e)} />
+          </label>
+          <button type='button' onClick={validEmail}>
+            인증코드받기
+          </button>
+          <label>
+            인증코드
+            <input type='text' name='code' onChange={(e) => hadleChange(e)} />
+          </label>
+          <button type='button'>인증하기</button>
+          <button>가입하기</button>
+        </form>
+      </Container>
     </>
   );
 }
--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -1,10 +1,37 @@
-const { User } = require('../models/user.js');
+const User = require('../models/user');
 
-// userIdvaild
-module.exports.userIdvaild = (req, res) => {
+// useridvaild
+const useridvaild = async (req, res) => {
   const { userId } = req.body;
   console.log(userId);
   //console.log(res);
+  try {
+    const user = await User.findOne({ userId: userId });
+    if (user) {
+      return res.status(200).json({ valid: false, message: '사용중인 아이디입니다.' });
+    }
+    return res.status(200).json({ valid: true, message: '사용 가능한 아이디입니다.' });
+  } catch (error) {
+    console.error('서버에러', error);
+    return res.status(500).json({ message: '서버 에러' });
+  }
+};
+
+// emailvalid
+const emailvalid = async (req, res) => {
+  const { email } = req.body;
+  console.log(email);
+  //console.log(res);
+  try {
+    const validEmail = await User.findOne({ email: email });
+    if (validEmail) {
+      return res.status(200).json({ valid: false, message: '사용중인 이메일입니다.' });
+    }
+    return res.status(200).json({ valid: true, message: '이메일로 인증코드가 발송되었습니다.' });
+  } catch (error) {
+    console.error('서버에러', error);
+    return res.status(500).json({ message: '서버 에러' });
+  }
 };
 
 // Signup
@@ -23,3 +50,5 @@ const signup = (req, res, next) => {
 };
 
 exports.signup = signup;
+exports.useridvaild = useridvaild;
+exports.emailvalid = emailvalid;
