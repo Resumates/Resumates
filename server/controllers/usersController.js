3fb56b4841b23cd04ac161e4f0ebdc96aa18fcdf
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-06-26 16:22:15 +0900

    Feat: 이메일 인증

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -94,6 +94,73 @@ const emailvalid = async (req, res) => {
   }
 };
 
+// 메일 인증
+const sendmail = () => async (req, res) => {
+  const { email } = req.body;
+
+  if (validation(email)) {
+    try {
+      const result = await send(email);
+
+      res.send({ message: '인증 메일을 발송했습니다.', result });
+    } catch (e) {
+      res.send({ e });
+    }
+  } else {
+    res.send({ message: '메일 전송에 실패했습니다.', data });
+  }
+};
+
+const send = async (data) => {
+  const transporter = nodeMailer.createTransport({
+    service: 'naver',
+    host: 'smtp.naver.com',
+    port: 587,
+    auth: {
+      user: `${process.env.ADMIN_EMAIL}`,
+      pass: `${process.env.ADMIN_PW}`,
+    },
+  });
+
+  const sendNotification = async (recipientEmail, code) => {
+    // 발신자, 수신자와 전송할 이메일 내용 설정
+    const BUSINESS_NAME = 'Resumates';
+    const mailOptions = {
+      from: `${BUSINESS_NAME} <${process.env.ADMIN_EMAIL}>`,
+      to: recipientEmail, // 수신자
+      subject: `${BUSINESS_NAME} : 회원가입 이메일 인증 메일입니다.`, // 제목
+      text: '', // 이메일 내용
+      html: `
+    <div>
+      <p>${BUSINESS_NAME}</p>
+      <p>Confirmation Code : ${code}</p>
+    </div>
+    `, // 이메일 본문에 html을 담아 전송
+    };
+    try {
+      await transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('error occured sending email:', error);
+        } else {
+          console.log('success:', info.response);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+const validation = (data) => {
+  return (
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      data.email,
+    ) &&
+    data.title != undefined &&
+    data.message != undefined
+  );
+};
+
 // 회원가입
 const signup = async (req, res) => {
   const { userId, userPw, email } = req.body;
@@ -116,6 +183,7 @@ const signup = async (req, res) => {
 
 exports.getUsers = getUsers;
 exports.login = login;
-exports.signup = signup;
 exports.useridvaild = useridvaild;
 exports.emailvalid = emailvalid;
+exports.sendmail = sendmail;
+exports.signup = signup;
