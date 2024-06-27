f6751358d87f97476e9e390f257604ef089ae8e3
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-06-27 11:14:38 +0900

    Feat: client 페이지 router 설정

---
+++ client/src/pages/resume/CreateResume.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CreateResume() {
+  return <div>CreateResume</div>;
+}
--- client/src/router/AppRoutes.js
+++ client/src/router/AppRoutes.js
@@ -1,9 +1,14 @@
 import React, { useCallback, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Signup from '../pages/auth/Signup';
-import Login from '../pages/auth/Login';
 import MainNavigation from '../components/Navigation/MainNavigation';
 import { AuthContext } from '../components/common/context/auth-context';
+import Signup from '../pages/auth/Signup';
+import Login from '../pages/auth/Login';
+import Main from '../pages/home/Main';
+import TemplateList from '../pages/resume/TemplateList';
+import CreateResume from '../pages/resume/CreateResume';
+import Mypage from '../pages/user/Mypage';
+import Account from '../pages/user/Account';
 
 export default function AppRoutes() {
   const [isLoggedIn, setLoggedIn] = useState(false);
@@ -20,8 +25,26 @@ export default function AppRoutes() {
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
       <MainNavigation />
       <Routes>
+        {/* 회원가입 */}
         <Route path='/user/signup' element={<Signup />} />
+
+        {/* 로그인 */}
         <Route path='/user/login' element={<Login />} />
+
+        {/* 메인 */}
+        <Route path='/' element={<Main />} />
+
+        {/* 템플릿 목록 */}
+        <Route path='/list' element={<TemplateList />} />
+
+        {/* 이력서 작성 */}
+        <Route path='/resume' element={<CreateResume />} />
+
+        {/* 마이페이지 */}
+        <Route path='/mypage' element={<Mypage />} />
+
+        {/* 계정 설정 */}
+        <Route path='/user/settings' element={<Account />} />
       </Routes>
     </AuthContext.Provider>
   );
