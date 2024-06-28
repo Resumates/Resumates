e912b5cfa505c67d9d610077082ada38a48747ae
hyonie <dev.hyonie@gmail.com>
2024-06-28 14:44:58 +0900

    Refactor: 이력서 작성 birth 필드 수정

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -4,20 +4,17 @@ const { Resumes } = require('../models/resumes');
 const resumeController = {
   // 이력서 작성
   async insertResume(req, res) {
-    let data = {
-      name: req.body.name,
-      image: req.body.image,
-      birth: req.body.birth,
-      gender: req.body.gender,
-      phone: req.body.phone,
-      email: req.body.email,
-    };
-
     try {
-      if (!req.body.name) return res.status(400).send({ err: 'name is required' });
-      const newResume = new Resumes(req.body);
+      let data = { ...req.body };
+      // 문자열을 Date객체로 변환
+      const birthData = new Date(data.birth);
+      birthData.setHours(0, 0, 0, 0);
+
+      if (!data.name) return res.status(400).send({ err: 'name is required' });
+
+      const newResume = new Resumes(data);
       await newResume.save();
-      return res.send({ '이력서 저장 성공': newResume });
+      return res.status(201).send({ '이력서 저장 성공': newResume });
     } catch (err) {
       return res.status(500).send({ '이력서 저장 실패': err.message });
     }
@@ -27,20 +24,31 @@ const resumeController = {
   async editResume(req, res) {
     try {
       const { resumeId } = req.params;
+
       if (!mongoose.isValidObjectId(resumeId))
         return res.status(400).send({ err: '유효한 ObjectId가 아닙니다.' });
-      const { name, image, gender, phone } = req.body;
-      if (!name && !image && !gender && !phone)
+
+      const { name, image, birth, gender, phone } = req.body;
+
+      // 문자열을 Date 객체로 변환
+      const birthDate = new Date(req.body.birth);
+      birthDate.setHours(0, 0, 0, 0);
+
+      req.body.birth = birthDate;
+
+      if (!name && !image && !birth && !gender && !phone)
         return res.status(400).send({ err: '정보를 수정하지 않았습니다.' });
 
       let updateBody = {};
       if (name) updateBody.name = name;
       if (image) updateBody.image = image;
+      if (birth) updateBody.birth = birth;
       if (gender) updateBody.gender = gender;
       if (phone) updateBody.phone = phone;
 
+      // 사용자 정보 업데이트
       const resume = await Resumes.findByIdAndUpdate(resumeId, updateBody, { new: true });
-      return res.send({ '이력서 수정': resume });
+      return res.status(200).send({ '이력서 수정': resume });
     } catch (err) {
       console.log(err);
       return res.status(500).send({ '이력서 수정 실패': err.message });
