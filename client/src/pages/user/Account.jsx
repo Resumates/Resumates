b45fffb764aaf33cfce3e379e76d9bf4878bf00f
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-06-29 14:59:53 +0900

    Refactor: 유저데이터 API 분리

--- client/src/pages/user/Account.jsx
+++ client/src/pages/user/Account.jsx
@@ -1,19 +1,7 @@
 import React, { useEffect } from 'react';
-import { getUserAccount } from '../../api/APIRoutes';
-import axios from 'axios';
+import { getUserData } from '../../api/commonAPI';
 
 export default function Account() {
-  
-  const getUserData = async (userId) => {
-    try {
-      const { data } = await axios.get(`${getUserAccount}/${userId}`);
-      console.log(data);
-    } catch (error) {
-      console.log('서버 error');
-      console.error('불러오기 오류', error);
-    }
-  };
-
   useEffect(() => {
     const userId = window.localStorage.getItem('userId');
     getUserData(userId);
