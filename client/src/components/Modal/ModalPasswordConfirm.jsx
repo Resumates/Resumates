94af51412c385abc1008debcca0576767fe79c6b
hyonie <dev.hyonie@gmail.com>
2024-07-01 11:03:03 +0900

    Feat: 모달 로그아웃

---
+++ client/src/components/Modal/ModalPasswordConfirm.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ModalPasswordConfirm() {
+  return <div>ModalPasswordConfirm</div>;
+}
--- client/src/pages/resume/CreateResume.jsx
+++ client/src/pages/resume/CreateResume.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ModalLogout from '../../components/Modal/ModalLogout';
 
 export default function CreateResume() {
-  return <div>CreateResume</div>;
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <div>
+      <button>로그아웃</button>
+      {setModalOpen && <ModalLogout setModalOpen={setModalOpen} />}
+      <br />
+      <button>비밀번호 확인</button>
+    </div>
+  );
 }
