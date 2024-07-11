b068166444bd693f7e675cb0f2e0ce13145e6008
hyonie <dev.hyonie@gmail.com>
2024-07-11 18:01:10 +0900

    Refactor: 스킬 추가버튼

--- client/src/components/resumeForm/InputField.jsx
+++ client/src/components/resumeForm/InputField.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { LabeledSelect, LabeledInput } from '../../style/CreateResumeStyle';
+import Button from '../common/Button';
 
 export function InputField({ label, type, name, placeholder, required }) {
   return (
     <LabeledInput className={name}>
       {label !== 'skillsBox' && <label htmlFor={name}>{label}</label>}
       <input type={type} name={name} placeholder={placeholder} required={required}></input>
+      {name == 'skill' && (
+        <Button type='button' padding='9px 8px' fontSize='16px'>
+          추가
+        </Button>
+      )}
     </LabeledInput>
   );
 }
 
 export function SelectField({ label, name, required, data, onOptionSelect, InfoId }) {
-  // const [selectedOption, setSelectedOPtion] = useState(null);
-
   const handleChange = (event) => {
     const selectedOption = event.target.value;
     if (InfoId === 'qualification') {
       onOptionSelect(selectedOption);
-      console.log('selectedOption', selectedOption);
     }
   };
 
