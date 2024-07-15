809000add4447309bb059293a03f079137903b36
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-07-15 11:20:11 +0900

    Style: 이미지 크롭 모달 UI 구현

--- client/src/pages/resume/SampleTemplate.jsx
+++ client/src/pages/resume/SampleTemplate.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ChangeTemplate from '../../components/resumeTamplate/ChangeTemplate';
+// import ChangeTemplate from '../../components/resumeTamplate/ChangeTemplate';
+import ModalCropImg from '../../components/Modal/ModalCropImg';
 
 export default function SampleTemplate() {
   return (
     <div>
       <div>SampleTemplate</div>
-      <ChangeTemplate />
+      {/* <ChangeTemplate /> */}
+      <ModalCropImg />
     </div>
   );
 }
