7f6722af9cca91707dd87be96ad4fce6b9d28316
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-07-21 13:30:35 +0900

    Style: 삭제 모달 UI 수정

--- client/src/components/account/EditUserInfo.jsx
+++ client/src/components/account/EditUserInfo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../common/Button';
 import {
   EditCont,
@@ -10,6 +10,7 @@ import {
   DelComment,
 } from './EditAccountStyle';
 import PropTypes from 'prop-types';
+import ModalDelete from '../Modal/ModalDelete';
 
 EditUserInfo.propTypes = {
   userInfo: PropTypes.object,
@@ -19,6 +20,16 @@ EditUserInfo.propTypes = {
 
 export default function EditUserInfo({ userInfo, setModalOpen, modalOpen }) {
   const { userId, email } = userInfo;
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
+  const handleDelete = async () => {
+    // const deleteAccount = await deleteResumeAPI(resumeId);
+    // if (deleteResume) {
+    //   alert('이력서가 삭제되었습니다.');
+    //   setSelectedResume(null);
+    //   window.scrollTo(0, 0);
+    // }
+  };
 
   return (
     <>
@@ -36,8 +47,11 @@ export default function EditUserInfo({ userInfo, setModalOpen, modalOpen }) {
             </Button>
           </InfoItem>
         </UserInfoSet>
-        <DeleteBtn>계정 삭제</DeleteBtn>
+        <DeleteBtn onClick={() => setDeleteModalOpen(!deleteModalOpen)}>계정 삭제</DeleteBtn>
         <DelComment>계정 삭제시 작성했던 모든 이력서가 삭제됩니다.</DelComment>
+        {deleteModalOpen && (
+          <ModalDelete setModalOpen={setDeleteModalOpen} handleDelete={handleDelete} />
+        )}
       </EditCont>
     </>
   );
