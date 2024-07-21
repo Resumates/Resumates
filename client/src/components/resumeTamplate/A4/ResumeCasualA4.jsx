aa940f719a1c6a1069765ddaab593fe301e0a67f
Sooooyeon <118328426+Sooooyeon@users.noreply.github.com>
2024-07-21 20:16:18 +0900

    Fix: casualA4 컴포넌트 데이터 출력

--- client/src/components/resumeTamplate/A4/ResumeCasualA4.jsx
+++ client/src/components/resumeTamplate/A4/ResumeCasualA4.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   A4Container,
   ProfileHeader,
@@ -7,6 +7,10 @@ import {
   ProfileInfo,
   UserInfo,
 } from './ResumeCasualA4Style.jsx';
+import birthImg from '../../../asset/images/icon-birth-casual.png';
+import EmailImg from '../../../asset/images/icon-email-casual.png';
+import PhoneImg from '../../../asset/images/icon-phone-casual.png';
+import HomeImg from '../../../asset/images/icon-addr-casual.png';
 import profileImg from '../../../asset/profile/blank-profile.webp';
 import CasualLineA4 from './CasualLineA4.jsx';
 
@@ -15,6 +19,9 @@ export default function CasualResume({ resumeDetail }) {
   const structure = resumeDetail?.structure;
   const content = resumeDetail?.structure.content;
   const { activity, certificate, portfolio, skills, workExperience } = content;
+  useEffect(() => {
+    console.log(content);
+  }, []);
 
   const birthday = birth?.slice(0, 10).replaceAll('-', '.');
 
@@ -26,18 +33,31 @@ export default function CasualResume({ resumeDetail }) {
           <Title>{structure.title}</Title>
           <UserInfo>
             <p className='name'>{name}</p>
-            <p>{birthday}</p>
-            <p>{email}</p>
-            <p>{phone}</p>
-            <p>{address}</p>
+            <p>
+              <img src={birthImg} alt='' />
+              {birthday}
+            </p>
+            <p>
+              <img src={EmailImg} alt='' />
+              {email}
+            </p>
+            <p>
+              <img src={PhoneImg} alt='' />
+              {phone}
+            </p>
+            <p>
+              <img src={HomeImg} alt='' />
+              {address}
+            </p>
           </UserInfo>
         </ProfileInfo>
       </ProfileHeader>
       <br></br>
-      <CasualLineA4 title={'기술스택'} content={resumeDetail.structure.content.skills} />
-      <CasualLineA4 title={'경력사항'} content={resumeDetail.structure.content.workExperience} />
-      <CasualLineA4 title={'학력'} />
-      <CasualLineA4 title={'포트폴리오'} content={resumeDetail.structure.content.portfolio.url} />
+      <CasualLineA4 title={'기술스택'} content={skills} />
+      <CasualLineA4 title={'경력사항'} content={workExperience} />
+      <CasualLineA4 title={'경험/활동/교육'} content={activity} />
+      <CasualLineA4 title={'자격/어학/수상'} content={certificate} />
+      <CasualLineA4 title={'포트폴리오'} content={portfolio.url} />
     </A4Container>
   );
 }
